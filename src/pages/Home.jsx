20c38da6b80b0af623eaebcab1d2e35c2f4879a6
Philippa Bather <bather1984@hotmail.com>
2023-01-31 21:33:42 +0100

    Initial uploads
    Initial project files added (index, app, html); initial folders added (src and pages).
    
    json and package-lock files added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../layout/Header";
+import NavigationPanel from "../layout/NavigationPanel";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <NavigationPanel />
+      <main></main>
+    </>
+  );
+};
+
+export default Home;
